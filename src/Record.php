962eeaf28edf2532f3b8d6530d839afbe2b4015a
rupeat <244753970@qq.com>
2019-01-31 11:13:57 +0800

    初步发包

---
+++ src/Record.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dell
+ * Date: 2019/1/31
+ * Time: 11:13
+ */
+
+namespace WirelessCognitive\LaravelOrm;
+
+
+class Record
+{
+    public function select(){
+        echo 'select';
+    }
+}
--- src/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: dell
- * Date: 2019/1/21
- * Time: 9:24
- */
-echo 'test success';
