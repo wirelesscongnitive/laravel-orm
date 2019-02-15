ac2269188faa628a15217aa6cd863ed149cda841
rupeat <244753970@qq.com>
2019-02-15 15:06:17 +0800

    更新包

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require  "./vendor/autoload.php";
+/**
+ * Created by PhpStorm.
+ * User: dell
+ * Date: 2019/2/15
+ * Time: 13:17
+ */
+$vueMaker = new \WirelessCognitive\LaravelOrm\ApiForVue();
+$vueMaker->make();
